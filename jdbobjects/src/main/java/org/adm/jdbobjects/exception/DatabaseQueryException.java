339c4d711a6156c72f10d37e0310d9d0e56a904d
Adrian Moldovan <adrian.moldovan00@gmail.com>
2015-04-07 13:34:32 +0300

    added headers;

--- jdbobjects/src/main/java/org/adm/jdbobjects/exception/DatabaseQueryException.java
+++ jdbobjects/src/main/java/org/adm/jdbobjects/exception/DatabaseQueryException.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright jDBObjects.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 3 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 package org.adm.jdbobjects.exception;
 
 
